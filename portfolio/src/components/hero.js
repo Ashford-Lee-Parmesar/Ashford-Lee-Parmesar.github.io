af1b012ffffdd4f8f6a81d88b64b0e5c518dbec7
Ashford Lee Parmesar <ashfordlee19@gmail.com>
2024-04-21 23:11:34 +0200

    Updated Hero Component
    I created a circular profile image card, using a placeholder image.
    
    TODO: Upload a real headshot of myself to replace the placeholder image.

--- portfolio/src/components/hero.js
+++ portfolio/src/components/hero.js
@@ -1,10 +1,11 @@
 import React from "react";
+import headshot from "../content/images/male_profile.svg"
 
 const Hero = () => {
   return (
     <hero>
       <div className="p-6 m-6 flex flex-col lg:flex-row lg:justify-between">
-        <div className="text-white   lg:w-2/4">
+        <div className="text-white lg:w-2/4">
           <div className="text-4xl lg:text-7xl mb-12">
             I<span className="text-red-400">'</span>m Ashford Lee Parmesar
             <span className="text-yellow-400">.</span>
@@ -14,11 +15,13 @@ const Hero = () => {
             digital experiences. Currently, I am honing my craft at <a href="#bidvestelectricalwebsite"className="text-blue-400 hover:text-blue-500">Bidvest
             Electrical</a>.
           </div>
-          <div className="text-lg lg:text-xl text-[#CCD0CF]">
+          <div className="text-lg lg:text-xl text-[#CCD0CF] mb-12">
           Where code meets creativity.
           </div>
         </div>
-        <div className="text-white  lg:w-1/4"></div>
+        <div className="lg:w-2/4 flex justify-center items-center">
+          <img className="border-[10px] rounded-full lg:h-[400px] lg:w-[400px] w-[250px] h-[250px]" src={headshot} alt="" />
+        </div>
       </div>
     </hero>
   );
